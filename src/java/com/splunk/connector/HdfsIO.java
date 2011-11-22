e0c78bdbe56d4240ef15fe07b49caf8f23af4152
Xiaoyuan Li <xiaoyuanli66@yahoo.com>
2011-11-21 17:44:58 -0800

    SPL-45295 Automatically pick up customed connector conf file

--- src/java/com/splunk/connector/HdfsIO.java
+++ src/java/com/splunk/connector/HdfsIO.java
@@ -24,7 +24,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.Path;;
+import org.apache.hadoop.fs.Path;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
