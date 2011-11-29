6a733a1b5b91fe5d6bbdee4028659606ae59d0f0
Emre Berge Ergenekon <eergenekon@splunk.com>
2011-11-28 22:23:19 -0800

    Unittest2 works as expected.
    The test has setup and also partial tear down. It compares the result with an expected value and behaves accordingly.

--- test/unittests/hadoopunittest2/runtest.sh
+++ test/unittests/hadoopunittest2/runtest.sh
@@ -17,6 +17,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$HADOOP_HOME/bin/hadoop jar $SPLBRANCH/build/jar/splunk_hadoop_unittests.jar com.splunk.mapreduce.lib.rest.tests.WordCount2 /wordcount/input /wordcount/output$1
-echo "Output from: hadoop dfs -cat /wordcount/output$1/part-00000 | tail -n 5"
-$HADOOP_HOME/bin/hadoop dfs -cat /wordcount/output$1/part-00000 | tail -n 5
+
+script_dir=$(dirname $0)
+HADOOP=$HADOOP_HOME/bin/hadoop
+SPLUNK=$SPLUNK_HOME/bin/splunk
+testFolder=/hadoopunittest2
+
+# Setup
+# TODO: The search is made because we don't clean splunk.
+#       The only code that belongs here is the splunk add oneshot wordfile-timestamp.
+#       Because if the file has been added twice, the test should fail.
+search=`splunk search 'source=*wordfile-timestamp | head 1'`
+if [ "$search" = "" ]; then
+  $SPLUNK add oneshot $script_dir/wordfile-timestamp
+fi
+
+# Test
+$HADOOP jar $SPLBRANCH/build/jar/splunk_hadoop_unittests.jar com.splunk.mapreduce.lib.rest.tests.WordCount2 "$testFolder/input" "$testFolder/output$1"
+
+expected_splunk_out="\
+2011-09-19	300
+a	300
+is	300
+test	300
+this	300"
+
+actual_splunk_out=$($HADOOP_HOME/bin/hadoop dfs -cat "$testFolder/output$1/part-00000" | tail -n 5)
+
+# Teardown
+$HADOOP fs -rmr "$testFolder" &>/dev/null
+# TODO clean splunk
+
+# Output
+if [ "$expected_splunk_out" != "$actual_splunk_out" ]
+then
+  echo "Fail!
+  Expected:
+  \"$expected_splunk_out\"
+  Actual:
+  \"$actual_splunk_out\""
+  exit 1
+else
+  exit 0
+fi
