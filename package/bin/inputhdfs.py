1a9279a9dbe069508275653427968d51de34ceeb
kbkaran <kiru@splunk.com>
2012-01-09 16:58:08 -0800

    job  and max, append options supported now. job option will not work until another checkin.

--- package/bin/inputhdfs.py
+++ package/bin/inputhdfs.py
@@ -12,73 +12,98 @@
 #   listed below in DEFAULT_ARGS can all be overridden as search arguments in
 #   the search string, i.e., 'inputhdfs separator=space file=hdfs://localhost:54310/myapp/outputtxtfile'.  From the
 #   current UI, prefix the search string with a pipe, i.e. '| inputhdfs <hdfs file url>'
-#
-#   append and max  options are not yet implemented
+#   when the append is set to True rows from the hdfs file or from the job output is appended to the piped in results
+#   max specifies the maximum number of rows that is output
 #
 
 import subprocess
 import os, sys, time, datetime
 import splunk.Intersplunk as isp
+import splunk.util as util
 
 DEFAULT_ARGS = {
-    'separator':	'space',
-	'file': 'nofile'
+    'separator': 'space',
+    'file': 'nofile',
+    'append': False,
+    'max': -1,
+    'input': 'noinput',
+    'output': 'nooutput',
+    'job': 'nojob'
 }
 
 # merge any passed args
 args = DEFAULT_ARGS
 for item in sys.argv:
-	kv = item.split('=')
-	if len(kv) > 1:
-		val = item[item.find('=') + 1:]
-		args[kv[0]] = val
+    kv = item.split('=')
+    if len(kv) > 1:
+        val = item[item.find('=') + 1:]
+        try:
+            val = int(val)
+        except:
+            pass
+        args[kv[0]] = util.normalizeBoolean(val)
 
 
-if args['file'] == 'nofile':	
- 	errorresult = splunk.Intersplunk.generateErrorResults("Usage: inputhdfs <separator=space|tab> <file=hdfsfilename>")
- 	splunk.Intersplunk.outputResults(errorresult)
-	sys.exit()
+def catfile(filename):
+    process = subprocess.Popen('java -cp $SPLUNK_HOME/etc/apps/shep/lib/hadoop-core-0.20.203.0.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-logging-1.1.1.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-configuration-1.6.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-lang-2.4.jar:$SPLUNK_HOME/etc/apps/shep/bin/splunk-hadoop-connector-0.4.1.jar com.splunk.shep.customsearch.HDFSCat ' + filename, shell=True, stdout=subprocess.PIPE)
+    return process
+
+if args['file'] == 'nofile' and args['job'] == 'nojob':    
+    errorresult = isp.generateErrorResults("Usage: inputhdfs <separator=space|tab> <file=hdfsfilename | job=mapredclassname> <max=#rows> <append=True|False>")
+    isp.outputResults(errorresult)
+    sys.exit()
 
 separator_char = ' '
 
 if args['separator'] == 'tab':
-	separator_char = '\t'
-
-process = subprocess.Popen('java -cp $SPLUNK_HOME/etc/apps/shep/lib/hadoop-core-0.20.203.0.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-logging-1.1.1.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-configuration-1.6.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-lang-2.4.jar:$SPLUNK_HOME/etc/apps/shep/bin/splunk-hadoop-connector-0.4.1.jar com.splunk.shep.customsearch.HDFSCat ' + args['file'], shell=True, stdout=subprocess.PIPE)
+    separator_char = '\t'
+if (args['file']) != 'nofile':
+    process = catfile(args['file'])
+if (args['job']) != 'nojob':
+    process = subprocess.Popen('java -cp $SPLUNK_HOME/etc/apps/shep/lib/commons-httpclient-3.0.jar:$SPLUNK_HOME/etc/apps/shep/lib/hadoop-core-0.20.205.0.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-logging-1.1.1.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-configuration-1.6.jar:$SPLUNK_HOME/etc/apps/shep/lib/commons-lang-2.4.jar:$SPLUNK_HOME/etc/apps/shep/bin/splunk-hadoop-connector-0.4.1.jar:/Users/kpakkirisamy/git/splunk-shep/build/jar/splunk_hadoop_unittests.jar:$SPLUNK_HOME/etc/apps/shep/lib/jackson-core-asl-1.4.0.jar:$SPLUNK_HOME/etc/apps/shep/lib/jackson-mapper-asl-1.4.0.jar:$SPLUNK_HOME/etc/apps/shep/lib/log4j-1.2.15.jar '
+                                    + args['job'] + " " + args['input'] + " " + args['output'], shell=True)
+    exitstatus = process.wait()
+    if (exitstatus == 0):
+        process = catfile(args['output']+'/part-00000')
 
 results = []
+if (args['append'] == True):
+    results,unused1,unused2 = isp.getOrganizedResults()
 chunkcount = 1;
 while True:
-	try :
-		line = process.stdout.readline()
-		if line:
-			rowset = {}
-			words = line.rstrip().split(separator_char)
-			rowset['_time']  = float(time.time())
-			rectime  = datetime.datetime.fromtimestamp(rowset['_time']).isoformat()	
-			raw = []
-			raw.append(rectime)
-			colcount = 0
-			for word in words:
-				rowset['col'+str(colcount)] = word.rstrip()	
-				colcount += 1
-				raw.append(word.rstrip())
-			rowset['_raw'] = separator_char.join(raw)
-			results.append(rowset)
-			chunkcount += 1
-			""" works only in Splunk 4.3
-			if chunkcount == 5:
-				isp.outputStreamResults(results)
-				chunkcount = 0
-				results = []
-			"""
-		else :
-			"""
-			if chunkcount > 0:
-				isp.outputStreamResults(results)
-			"""
-			isp.outputResults(results)
-			sys.exit()
-	except IOError:
-		sys.exit()
-	
+    try :
+        line = process.stdout.readline()
+        if line:
+            rowset = {}
+            words = line.rstrip().split(separator_char)
+            rowset['_time']  = float(time.time())
+            rectime  = datetime.datetime.fromtimestamp(rowset['_time']).isoformat()    
+            raw = []
+            raw.append(rectime)
+            colcount = 0
+            for word in words:
+                rowset['col'+str(colcount)] = word.rstrip()    
+                colcount += 1
+                raw.append(word.rstrip())
+            rowset['_raw'] = separator_char.join(raw)
+            results.append(rowset)
+            chunkcount += 1
+            if ((args['max'] != -1) and (chunkcount > args['max'])):
+                    isp.outputResults(results)
+                    break
+#            works only in Splunk 4.3
+#            if chunkcount == 5:
+#                isp.outputStreamResults(results)
+#                chunkcount = 0
+#                results = []
+        else :
+#            works only in 4.3
+#            if chunkcount > 0:
+#                isp.outputStreamResults(results)
+
+            isp.outputResults(results)
+            sys.exit(0)
+    except IOError:
+        sys.exit(1)
+sys.exit(0)
+    
