f310369f5dd9cd3cadfab9ed0b4723d5f7d73aa6
kbkaran <kiru@splunk.com>
2012-01-17 19:27:16 -0800

    sh script to make up classpath independent of jar versions

---
+++ package/bin/hdfsrun.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# hdfscat.sh
+#
+# Copyright (C) 2011 Splunk Inc.
+#
+# Splunk Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# script to make jar loading independent of versions
+
+HADOOP_JAR=`ls ../lib/hadoop-core*.jar`
+CMNSLGNG=`ls ../lib/commons-logging*.jar`
+CMNSCNFG=`ls ../lib/commons-configuration*.jar`
+CMNSLANG=`ls ../lib/commons-lang*.jar`
+HADOOP_CLIENT_JARS=$HADOOP_JAR:$CMNSLGNG:$CMNSCNFG:$CMNSLANG
+CMNSHTTP=`ls ../lib/commons-httpclient*.jar`
+JKSNCORE=`ls ../lib/jackson-core-asl*.jar`
+JKSNMAPR=`ls ../lib/jackson-mapper-asl*.jar`
+LOG4J=`ls ../lib/log4j*.jar`
+SPLUNK_JARS=./*
+HADOOP_LAUNCH_JARS=$CMNSHTTP:$JKSNCORE:$JKSNMAPR:$LOG4J
+java -cp $HADOOP_CLIENT_JARS:$HADOOP_LAUNCH_JARS:$SPLUNK_JARS $@
