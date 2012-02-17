cb4800874c1d5fb45db0b2342f4d0174e35523b7
Petter Eriksson <periksson@splunk.com>
2012-02-16 17:03:24 -0800

    MethodCallerHelper is now tested for multiple inner calls before calling getCallerOfMyMethod().

--- test/java/com/splunk/shep/testutil/MethodCallerHelper.java
+++ test/java/com/splunk/shep/testutil/MethodCallerHelper.java
@@ -2,9 +2,30 @@ package com.splunk.shep.testutil;
 
 public class MethodCallerHelper {
 
+    private final int INDEX_OF_CALLER_TO_THIS_METHOD = 3;
+
     public StackTraceElement getCallerToMyMethod() {
-	int indexOfCallerToTheirMethod = 4;
-	return Thread.getAllStackTraces().get(Thread.currentThread())[indexOfCallerToTheirMethod];
+	StackTraceElement[] elements = Thread.getAllStackTraces().get(
+		Thread.currentThread());
+	return getCallerToMyMethod(elements);
+    }
+
+    protected StackTraceElement getCallerToMyMethod(StackTraceElement[] elements) {
+	StackTraceElement caller = elements[INDEX_OF_CALLER_TO_THIS_METHOD];
+	return getClassThatCalledCaller(elements, caller);
+    }
+
+    private StackTraceElement getClassThatCalledCaller(
+	    StackTraceElement[] elements, StackTraceElement caller) {
+	for (int i = INDEX_OF_CALLER_TO_THIS_METHOD; i < elements.length; i++)
+	    if (!isSameClassAsCaller(caller, elements[i]))
+		return elements[i];
+	return null;
+    }
+
+    private boolean isSameClassAsCaller(StackTraceElement caller,
+	    StackTraceElement element) {
+	return element.getClassName().equals(caller.getClassName());
     }
 
 }
--- test/java/com/splunk/shep/testutil/MethodCallerHelperTest.java
+++ test/java/com/splunk/shep/testutil/MethodCallerHelperTest.java
@@ -6,14 +6,27 @@ import org.testng.annotations.Test;
 
 public class MethodCallerHelperTest {
 
-
     @Test(groups = { "fast" })
     public void getCallerToMyMethod_should_return_stackTraceElement_with_thisClass_when_invokingCaller_call() {
 	StackTraceElement element = new Caller().call();
 	assertEquals(element.getClassName(), getClass().getName());
     }
 
+    @Test(groups = { "fast" })
+    public void getCallerToMyMethod_should_stillGetThisClass_when_otherInternalCallsInsideTheClassIsCalled() {
+	StackTraceElement element = new Caller().internalCall_before_call();
+	assertEquals(element.getClassName(), getClass().getName());
+    }
+
     private static class Caller {
+
+	public StackTraceElement internalCall_before_call() {
+	    return internalCall();
+	}
+
+	public StackTraceElement internalCall() {
+	    return call();
+	}
 
 	public StackTraceElement call() {
 	    return new MethodCallerHelper().getCallerToMyMethod();
