67847a20237ab5025722a8a9c0668e7b1e6209f8
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-02-17 13:39:37 -0800

    Created the file system interface.

---
+++ src/java/com/splunk/shep/archiver/FileSystem.java
@@ -0,0 +1,10 @@
+package com.splunk.shep.archiver;
+
+/**
+ * With this interface code can send, get, list files in any system that
+ * emulates a file system.
+ * 
+ */
+public interface FileSystem {
+
+}
