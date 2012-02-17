044b4dc673074390d87b4e3e89c6a1d728cf58f1
Petter Eriksson <periksson@splunk.com>
2012-02-16 17:50:56 -0800

    Extracted getLocalFileSystem() to a FileSystemUtils class.

--- test/java/com/splunk/shep/testutil/SafePathCreatorTest.java
+++ test/java/com/splunk/shep/testutil/SafePathCreatorTest.java
@@ -2,9 +2,6 @@ package com.splunk.shep.testutil;
 
 import static org.testng.Assert.assertEquals;
 
-import java.io.IOException;
-
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.testng.annotations.BeforeTest;
@@ -21,18 +18,10 @@ public class SafePathCreatorTest {
 
     @Test(groups = { "fast" })
     public void safePath_should_beSeparated_by_HomeDirectoryAndNameOfTestCase_toAchieve_nicerStructure() {
-	FileSystem fileSystem = getLocalFileSystem();
+	FileSystem fileSystem = FileSystemUtils.getLocalFileSystem();
 	Path safePath = safePathCreator.getPathOnFileSystem(fileSystem);
 	Path expected = new Path(fileSystem.getHomeDirectory() + "/"
 		+ this.getClass().getName());
 	assertEquals(safePath, expected);
-    }
-
-    private FileSystem getLocalFileSystem() {
-	try {
-	    return FileSystem.getLocal(new Configuration());
-	} catch (IOException e) {
-	    throw new RuntimeException("Couldn't create local file system", e);
-	}
     }
 }
