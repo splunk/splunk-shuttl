fb235dcd358ac81b542ed9439eae12d731720e8e
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-02-17 15:44:14 -0800

    Renamed FilesSystem interface to ArchiveFileSystem since its an interface for cominicating on the remote file system

---
+++ src/java/com/splunk/shep/archiver/fileSystem/ArchiveFileSystem.java
@@ -0,0 +1,9 @@
+package com.splunk.shep.archiver.fileSystem;
+
+/**
+ * With this interface code can send, get, list files in any system that is used
+ * for to archiving.
+ * 
+ */
+public interface ArchiveFileSystem {
+}
