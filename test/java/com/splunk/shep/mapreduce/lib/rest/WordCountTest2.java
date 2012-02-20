c3caef65f16e999bb72bebc4a9776fb1bd630a8e
Petter Eriksson <periksson@splunk.com>
2012-02-19 19:42:40 -0800

    Begun working on WordCountTest2.
    It only oneshots a file to splunk if it's not
    already oneshotted.

---
+++ test/java/com/splunk/shep/mapreduce/lib/rest/WordCountTest2.java
@@ -0,0 +1,69 @@
+package com.splunk.shep.mapreduce.lib.rest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.splunk.Job;
+import com.splunk.Service;
+
+public class WordCountTest2 {
+
+    @Test(groups = { "slow" })
+    @Parameters({ "splunk.host", "splunk.mgmtport", "splunk.username",
+	    "splunk.password", "splunk.home" })
+    public void wordCountTest2_setUp(String splunkHost, String splunkMGMTPort,
+	    String splunkUsername, String splunkPassword, String splunkHome)
+	    throws InterruptedException, IOException {
+
+	Service splunk = getLoggedInSplunkService(splunkHost, splunkMGMTPort,
+		splunkUsername, splunkPassword);
+	if (!isTestFileAlreadyIndexed(splunk))
+	    indexTestFile(splunkHome);
+
+    }
+
+    private void indexTestFile(String splunkHome) throws IOException,
+	    InterruptedException {
+	// There's currently no way to oneshot a file through the SDK.
+	// Using splunk.home instead.
+	File file = new File("test/java/com/splunk/shep/mapreduce/lib/rest"
+		+ "/wordfile-timestamp");
+	Process exec = Runtime.getRuntime().exec(
+		splunkHome + "/bin/splunk add oneshot "
+			+ file.getAbsolutePath());
+	System.out.println("exec.waitFor() " + exec.waitFor());
+	System.out.println("splunk add oneshot out: "
+		+ IOUtils.readLines(exec.getInputStream()));
+	System.out.println("splunk add oneshot err: "
+		+ IOUtils.readLines(exec.getErrorStream()));
+    }
+
+    private boolean isTestFileAlreadyIndexed(Service splunk)
+	    throws InterruptedException {
+	Job search = splunk.getJobs().create(
+		"search index=main source=*wordfile-timestamp");
+	waitWhileSearchFinishes(search);
+	return search.getResultCount() > 0;
+    }
+
+    private void waitWhileSearchFinishes(Job search)
+	    throws InterruptedException {
+	while (!search.isDone()) {
+	    Thread.sleep(10);
+	    search.refresh();
+	}
+    }
+
+    private Service getLoggedInSplunkService(String splunkHost,
+	    String splunkMGMTPort, String splunkUsername, String splunkPassword) {
+	Service splunk = new Service(splunkHost,
+		Integer.parseInt(splunkMGMTPort));
+	splunk.login(splunkUsername, splunkPassword);
+	return splunk;
+    }
+
+}
