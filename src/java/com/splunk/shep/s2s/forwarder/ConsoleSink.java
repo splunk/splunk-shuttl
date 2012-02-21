eb1f1104b4493e61c3259101834ae51e791145cd
kbkaran <kiru@splunk.com>
2012-02-21 12:31:02 -0800

    Console logs at debug level. Turn it into a FileSink using log4j.properties config.

--- src/java/com/splunk/shep/s2s/forwarder/ConsoleSink.java
+++ src/java/com/splunk/shep/s2s/forwarder/ConsoleSink.java
@@ -25,10 +25,9 @@ import com.splunk.shep.s2s.DataSink;
  */
 public class ConsoleSink implements DataSink {
 
-    private Logger logger = Logger.getLogger(getClass());
+    private Logger logger = Logger.getLogger("ConsoleSink");
 
     public ConsoleSink() {
-	logger.info("init");
     }
 
     @Override
@@ -38,28 +37,22 @@ public class ConsoleSink implements DataSink {
 
     @Override
     public void start() throws Exception {
-	// TODO Auto-generated method stub
-	logger.info("start");
     }
 
     @Override
     public void start(String sinkPath) throws Exception {
-	// TODO Auto-generated method stub
-	logger.info("start");
     }
 
     @Override
     public void close() {
-	// TODO Auto-generated method stub
-	logger.info("close");
     }
 
     @Override
     public void send(byte[] rawBytes, String sourceType, String source,
 	    String host, long time) throws Exception {
-	// TODO Auto-generated method stub
-	System.out.println("bytes " + new String(rawBytes, "UTF-8"));
-	System.out.println("bytes " + sourceType + " " + source + " " + host
+	logger.debug("ConsoleSink: " + "bytes " + new String(rawBytes, "UTF-8"));
+	logger.debug("ConsoleSink: " + "bytes " + sourceType + " " + source
+		+ " " + host
 		+ " " + time);
 
     }
@@ -67,29 +60,25 @@ public class ConsoleSink implements DataSink {
     @Override
     public void send(String data, String sourceType, String source,
 	    String host, long time) throws Exception {
-	// TODO Auto-generated method stub
-	System.out.println("bytes str:" + data);
-	System.out.println("bytes str" + sourceType + " " + source + " " + host
+	logger.debug("ConsoleSink: " + "bytes str:" + data);
+	logger.debug("ConsoleSink: " + "bytes str" + sourceType + " " + source
+		+ " " + host
 		+ " " + time);
     }
 
     @Override
     public void send(byte[] rawBytes) throws Exception {
-	// TODO Auto-generated method stub
-	System.out.println("byte[] " + new String(rawBytes, "UTF-8"));
-
+	logger.debug("ConsoleSink: " + "byte[] "
+		+ new String(rawBytes, "UTF-8"));
     }
 
     @Override
     public void setMaxEventSize(long size) {
-	// TODO Auto-generated method stub
 
     }
 
     @Override
     public void setFileRollingSize(long size) {
-	// TODO Auto-generated method stub
-
     }
 
 }
