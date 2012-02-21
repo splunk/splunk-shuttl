3465611afdfe870a186b794872f1f68ffedb911f
Petter Eriksson <periksson@splunk.com>
2012-02-21 02:55:43 -0800

    Removed conditional code from HDFSCat. It's easier now.

--- src/java/com/splunk/shep/customsearch/HDFSCat.java
+++ src/java/com/splunk/shep/customsearch/HDFSCat.java
@@ -16,39 +16,23 @@ public class HDFSCat {
      */
     public static void main(String args[]) {
 	Configuration conf = new Configuration();
-	FileSystem fs;
 	Path filenamePath = new Path(args[0]);
 	URI fileuri = URI.create(args[0]);
+	FileSystem fs = getFileSystem(fileuri, conf);
 
-	fs = getFileSystemDepedningOnURISchema(conf, fileuri);
 	verifyFileExistance(filenamePath, fs);
 	printContentsOfPathToSystemOut(fs, filenamePath);
 	System.exit(0);
     }
 
-    private static FileSystem getFileSystemDepedningOnURISchema(
-	    Configuration conf, URI fileuri) {
+    private static FileSystem getFileSystem(URI fileuri, Configuration conf) {
 	try {
-	    return doGetFileSystem(conf, fileuri);
+	    return FileSystem.get(fileuri, conf);
 	} catch (IOException e) {
 	    e.printStackTrace();
 	    System.exit(2);
-	    return null; // Won't happen, because of System.exit.
+	    return null;
 	}
-    }
-
-    private static FileSystem doGetFileSystem(Configuration conf, URI fileuri)
-	    throws IOException {
-	FileSystem fs;
-	if (fileuri.getScheme().equals("file")) {
-	    fs = FileSystem.getLocal(conf);
-	} else {
-	    String host = fileuri.getHost();
-	    int port = fileuri.getPort();
-	    fs = FileSystem
-		    .get(URI.create("hdfs://" + host + ":" + port), conf);
-	}
-	return fs;
     }
 
     private static void verifyFileExistance(Path filenamePath, FileSystem fs) {
