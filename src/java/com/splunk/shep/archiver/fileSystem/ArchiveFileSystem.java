2d9430748be3f13b86ef64bcdad276b838e3d465
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-02-22 12:39:52 -0800

    Defined the putFile method on ArchiveFileSystem interface

--- src/java/com/splunk/shep/archiver/fileSystem/ArchiveFileSystem.java
+++ src/java/com/splunk/shep/archiver/fileSystem/ArchiveFileSystem.java
@@ -1,5 +1,8 @@
 package com.splunk.shep.archiver.fileSystem;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * With this interface code can put, retrieve and list files in any system that
  * is used for to archiving.
@@ -7,4 +10,21 @@ package com.splunk.shep.archiver.fileSystem;
  */
 public interface ArchiveFileSystem {
 
+    /**
+     * Puts the specified file on local file system to the archiving file
+     * system.
+     * 
+     * @param fileOnLocalFileSystem
+     *            An existing file on the local file system.
+     * @param fileOnArchiveFileSystem
+     *            A non exiting file on the archive file system.
+     * 
+     * @throws FileNotFoundException
+     *             If specified file on the local file system doesn't exist.
+     * @throws FileAlreadyExistsException
+     *             If the specified file on archive file system already exists.
+     */
+    void putFile(File fileOnLocalFileSystem,
+	    File fileOnArchiveFileSystem) throws FileNotFoundException,
+	    FileAlreadyExistsException;
 }
---
+++ src/java/com/splunk/shep/archiver/fileSystem/FileAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.splunk.shep.archiver.fileSystem;
+
+import java.io.IOException;
+
+public class FileAlreadyExistsException extends IOException {
+
+    private static final long serialVersionUID = 1L;
+
+}
