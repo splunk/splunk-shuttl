f62086dbf098d64c708edfc305229b3e2b520401
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-02-22 09:57:30 -0800

    Fixed a typo on the comments

--- src/java/com/splunk/shep/archiver/fileSystem/ArchiveFileSystem.java
+++ src/java/com/splunk/shep/archiver/fileSystem/ArchiveFileSystem.java
@@ -1,9 +1,10 @@
 package com.splunk.shep.archiver.fileSystem;
 
 /**
- * With this interface code can send, get, list files in any system that is used
- * for to archiving.
+ * With this interface code can put, retrieve and list files in any system that
+ * is used for to archiving.
  * 
  */
 public interface ArchiveFileSystem {
+
 }
