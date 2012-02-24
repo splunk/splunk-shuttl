1b82d07666ef88aebf4162948320c74231e298ff
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-02-23 20:33:26 -0800

    Changed FileAlreadyExistsException to be FileOverwriteException. Made the file system interface use FileSystemPath instead of File and fixed comments

--- src/java/com/splunk/shep/archiver/fileSystem/ArchiveFileSystem.java
+++ src/java/com/splunk/shep/archiver/fileSystem/ArchiveFileSystem.java
@@ -17,26 +17,34 @@ public interface ArchiveFileSystem {
      * @param fileOnLocalFileSystem
      *            An existing file on the local file system.
      * @param fileOnArchiveFileSystem
-     *            A non exiting file on the archive file system.
+     *            Path pointing for an non exiting file on the archive file
+     *            system.
      * 
      * @throws FileNotFoundException
      *             If specified file on the local file system doesn't exist.
-     * @throws FileAlreadyExistsException
-     *             If the specified file on archive file system already exists.
+     * @throws FileOverwriteException
+     *             If there is already a file on the specified path.
      */
     void putFile(File fileOnLocalFileSystem,
-	    File fileOnArchiveFileSystem) throws FileNotFoundException,
-	    FileAlreadyExistsException;
+	    FileSystemPath fileOnArchiveFileSystem)
+	    throws FileNotFoundException, FileOverwriteException;
     
     /**
-     * Retries the file on archiving file system and stores it to the specified
-     * file on local file system.
+     * Retries the file from specified path on archiving file system and stores
+     * it to the specified file on local file system.
      * 
      * @param fileOnLocalFileSystem
-     *            A
+     *            A non exiting file on the local file system.
      * @param fileOnArchiveFileSystem
+     *            A path to an existing file on the archiving file system.
      * @throws FileNotFoundException
+     *             If there isn't a file on the archiving file system with the
+     *             specified path.
+     * @throws FileOverwriteException
+     *             If there is already a file on the local file system.
      */
-    void getFile(File fileOnLocalFileSystem, File fileOnArchiveFileSystem)
-	    throws FileNotFoundException;
+    void getFile(File fileOnLocalFileSystem,
+	    FileSystemPath fileOnArchiveFileSystem)
+	    throws FileNotFoundException, FileOverwriteException;
+    
 }
--- src/java/com/splunk/shep/archiver/fileSystem/FileOverwriteException.java
+++ src/java/com/splunk/shep/archiver/fileSystem/FileOverwriteException.java
@@ -2,7 +2,7 @@ package com.splunk.shep.archiver.fileSystem;
 
 import java.io.IOException;
 
-public class FileAlreadyExistsException extends IOException {
+public class FileOverwriteException extends IOException {
 
     private static final long serialVersionUID = 1L;
 
