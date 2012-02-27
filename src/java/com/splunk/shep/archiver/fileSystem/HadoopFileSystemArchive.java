74d68c90ed072481147a71f8d6911a48e14d7ce4
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-02-26 19:21:37 -0800

    Implemented getFile method of HadoopFileStystemArchive

--- src/java/com/splunk/shep/archiver/fileSystem/HadoopFileSystemArchive.java
+++ src/java/com/splunk/shep/archiver/fileSystem/HadoopFileSystemArchive.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 public class HadoopFileSystemArchive implements ArchiveFileSystem {
 
@@ -17,24 +18,39 @@ public class HadoopFileSystemArchive implements ArchiveFileSystem {
     }
 
     @Override
-    public void putFile(File fileOnLocalFileSystem,
- URI fileOnArchiveFileSystem)
+    public void putFile(File fileOnLocalFileSystem, URI fileOnArchiveFileSystem)
 	    throws FileNotFoundException, FileOverwriteException, IOException {
 
     }
 
     @Override
-    public void getFile(File fileOnLocalFileSystem,
- URI fileOnArchiveFileSystem)
+    public void getFile(File fileOnLocalFileSystem, URI fileOnArchiveFileSystem)
 	    throws FileNotFoundException, FileOverwriteException, IOException {
-	// TODO Auto-generated method stub
-
+	throwExceptionIfFileAllreadyExist(fileOnLocalFileSystem);
+	Path localPath = createPathFromFile(fileOnLocalFileSystem);
+	Path hadoopPath = createPathFromURI(fileOnArchiveFileSystem);
+	hadoopFileSystem.copyToLocalFile(hadoopPath, localPath);
     }
 
     @Override
-    public List<URI> listPath(URI pathToBeListed)
-	    throws IOException {
+    public List<URI> listPath(URI pathToBeListed) throws IOException {
 	// TODO Auto-generated method stub
 	return null;
+    }
+
+    private Path createPathFromURI(URI uri) {
+	return new Path(uri);
+    }
+
+    private Path createPathFromFile(File file) {
+	return createPathFromURI(file.toURI());
+    }
+
+    private void throwExceptionIfFileAllreadyExist(File file)
+	    throws FileOverwriteException {
+	if (file.exists()) {
+	    throw new FileOverwriteException(file.toString()
+		    + " allready exist.");
+	}
     }
 }
