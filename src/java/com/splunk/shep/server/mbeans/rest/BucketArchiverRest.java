64649c294aa0194dcccfa5cd272dd5f4e3cdd45f
Petter Eriksson <ipetterik@gmail.com>
2012-02-27 03:06:11 -0800

    The REST end point runs the BucketArchiver on a different thread.
    This way it returns back to the caller
    of the REST end point, so it doesn't
    block the script. Splunk can now delete
    the bucket if it wants to.

--- src/java/com/splunk/shep/server/mbeans/rest/BucketArchiverRest.java
+++ src/java/com/splunk/shep/server/mbeans/rest/BucketArchiverRest.java
@@ -1,10 +1,16 @@
 package com.splunk.shep.server.mbeans.rest;
 
+import java.io.FileNotFoundException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import com.splunk.shep.archiver.archive.BucketArchiver;
+import com.splunk.shep.archiver.model.Bucket;
+import com.splunk.shep.archiver.model.FileNotDirectoryException;
 
 /**
  * REST endpoint for archiving a bucket.
@@ -23,8 +29,34 @@ public class BucketArchiverRest {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/bucket/archive")
     public void archiveBucket(@QueryParam("path") String path) {
-	// Bucket bucket = new Bucket();
-	// BucketArchiver.create().archiveBucket(bucket);
-	throw new UnsupportedOperationException();
+	archiveBucketOnAnotherThread(path);
+    }
+
+    private void archiveBucketOnAnotherThread(String path) {
+	Runnable r = new BucketArchiverRunner(path);
+	new Thread(r).run();
+    }
+
+    private static class BucketArchiverRunner implements Runnable {
+
+	private final String pathToBucket;
+
+	public BucketArchiverRunner(String pathToBucket) {
+	    this.pathToBucket = pathToBucket;
+	}
+
+	@Override
+	public void run() {
+	    try {
+		BucketArchiver.create().archiveBucket(
+			Bucket.createWithAbsolutePath(pathToBucket));
+	    } catch (FileNotFoundException e) {
+		e.printStackTrace();
+		throw new RuntimeException(e);
+	    } catch (FileNotDirectoryException e) {
+		e.printStackTrace();
+		throw new RuntimeException(e);
+	    }
+	}
     }
 }
