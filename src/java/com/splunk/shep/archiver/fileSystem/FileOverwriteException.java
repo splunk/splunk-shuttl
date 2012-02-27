201403f02900a71dff00bd8c6d40aabd17721260
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-02-26 19:19:59 -0800

    Added constructors to FileOverwriteException to call the constructors of the super class.

--- src/java/com/splunk/shep/archiver/fileSystem/FileOverwriteException.java
+++ src/java/com/splunk/shep/archiver/fileSystem/FileOverwriteException.java
@@ -6,4 +6,11 @@ public class FileOverwriteException extends IOException {
 
     private static final long serialVersionUID = 1L;
 
+    public FileOverwriteException() {
+	super();
+    }
+
+    public FileOverwriteException(String message) {
+	super(message);
+    }
 }
