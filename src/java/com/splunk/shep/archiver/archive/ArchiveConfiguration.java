949d2c1c31650478055c39c62b7980084e371982
Petter Eriksson <periksson@splunk.com>
2012-02-26 22:43:32 -0800

    Implemented the stubs in ArchiveConfiguration.

--- src/java/com/splunk/shep/archiver/archive/ArchiveConfiguration.java
+++ src/java/com/splunk/shep/archiver/archive/ArchiveConfiguration.java
@@ -2,20 +2,23 @@ package com.splunk.shep.archiver.archive;
 
 public class ArchiveConfiguration {
 
+    private static final String ARCHIVING_ROOT = "archiving_root";
+    private static final String CLUSTER_NAME = "cluster_name";
+    private static final String SERVER_NAME = "server_name";
+
     public BucketFormat getArchiveFormat() {
 	return BucketFormat.SPLUNK_BUCKET;
     }
 
     public String getArchivingRoot() {
-	throw new UnsupportedOperationException();
+	return ARCHIVING_ROOT;
     }
 
     public String getClusterName() {
-	throw new UnsupportedOperationException();
+	return CLUSTER_NAME;
     }
 
     public String getServerName() {
-	throw new UnsupportedOperationException();
+	return SERVER_NAME;
     }
-
 }
