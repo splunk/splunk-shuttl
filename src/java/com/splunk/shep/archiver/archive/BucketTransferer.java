2ee63e3033dea872158f1de58f24a7a5a13c3f25
Petter Eriksson <ipetterik@gmail.com>
2012-02-27 02:04:18 -0800

    Created bucket transfer test.
    It also made me write the last of
    the construction code in BucketArchiver
    for completing the archiving.

--- src/java/com/splunk/shep/archiver/archive/BucketTransferer.java
+++ src/java/com/splunk/shep/archiver/archive/BucketTransferer.java
@@ -1,12 +1,33 @@
 package com.splunk.shep.archiver.archive;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
 
+import com.splunk.shep.archiver.fileSystem.ArchiveFileSystem;
+import com.splunk.shep.archiver.fileSystem.FileOverwriteException;
 import com.splunk.shep.archiver.model.Bucket;
 
 public class BucketTransferer {
 
+    private final ArchiveFileSystem archiveFileSystem;
+
+    public BucketTransferer(ArchiveFileSystem archive) {
+	archiveFileSystem = archive;
+    }
+
     public void transferBucketToArchive(Bucket bucket, URI path) {
-	throw new UnsupportedOperationException();
+	try {
+	    archiveFileSystem.putFile(bucket.getDirectory(), path);
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (FileOverwriteException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	}
     }
 }
