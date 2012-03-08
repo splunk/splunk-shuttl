7232bbb2402d688df31b435cf233c88d26110323
Petter Eriksson <periksson@splunk.com>
2012-03-08 01:14:00 -0800

    Separated tests for BucketFreezers.
    There are some tests that require one set of setup
    and other tests that require another setup. These
    are separated into different test classes.
    Some creation code of directories are also cleaned
    up now that we have UtilsFile.

--- test/java/com/splunk/shep/archiver/archive/BucketFreezerTest.java
+++ test/java/com/splunk/shep/archiver/archive/BucketFreezerTest.java
@@ -1,7 +1,6 @@
 package com.splunk.shep.archiver.archive;
 
 import static com.splunk.shep.testutil.UtilsFile.*;
-import static org.mockito.Mockito.*;
 import static org.testng.AssertJUnit.*;
 
 import java.io.File;
@@ -20,90 +19,17 @@ import com.splunk.shep.testutil.UtilsMockito;
 @Test(groups = { "fast" })
 public class BucketFreezerTest {
 
-    private Runtime runtimeMock;
     private HttpClient okReturningHttpClient = null;
 
     @BeforeClass
     public void beforeClass() throws ClientProtocolException, IOException {
 	okReturningHttpClient = UtilsMockito
 		.createAlwaysOKReturningHTTPClientMock();
-	runtimeMock = mock(Runtime.class);
     }
 
     @AfterMethod(groups = { "fast" })
     public void tearDownFast() {
-	reset(runtimeMock);
-	deleteDirectory(getTestDirectory());
-	deleteDirectory(new File(getSafeLocation()));
-    }
-
-    private void deleteDirectory(File dir) {
-	try {
-	    FileUtils.deleteDirectory(dir);
-	} catch (IOException e) {
-	    e.printStackTrace();
-	    throw new RuntimeException(e);
-	}
-    }
-
-    public void testDirectory_shouldNot_exist() {
-	assertFalse(getTestDirectory().exists());
-    }
-
-    public void main_existingDirecotry_returnCode0() throws IOException {
-	File directory = createTestDirectory();
-	runMainWithDepentencies_withArguments("index-name",
-		directory.getAbsolutePath());
-	verify(runtimeMock).exit(0);
-	// This deletion assumes and knows too much. When we remove
-	// BucketFreezer, this will be ok tho.
-	FileUtils
-		.deleteDirectory(new File(BucketFreezer.DEFAULT_SAFE_LOCATION));
-    }
-
-    public void main_noArguments_returnCodeMinus1() {
-	runMainWithDepentencies_withArguments();
-	verify(runtimeMock).exit(-1);
-    }
-
-    public void main_oneArgument_returnCodeMinus1() {
-	runMainWithDepentencies_withArguments("index-name");
-	verify(runtimeMock).exit(-1);
-    }
-
-    public void main_threeArguments_returnCodeMinus1() {
-	runMainWithDepentencies_withArguments("index-name", "/path/to/file",
-		"too-many-arguments");
-	verify(runtimeMock).exit(-1);
-    }
-
-    public void main_tooManyArguments_returnCodeMinus1() {
-	runMainWithDepentencies_withArguments("index-name", "/path/to/file",
-		"too-many-arguments", "too", "many", "arguments");
-	verify(runtimeMock).exit(-1);
-    }
-
-    public void main_fileNotADirectory_returnCodeMinus2() throws IOException {
-	File file = File.createTempFile("ArchiveTest", ".tmp");
-	file.deleteOnExit();
-	assertTrue(!file.isDirectory());
-	runMainWithDepentencies_withArguments("index-name",
-		file.getAbsolutePath());
-	verify(runtimeMock).exit(-2);
-    }
-
-    public void main_nonExistingFile_returnMinus3() {
-	File file = UtilsFile.createTestFilePath();
-	runMainWithDepentencies_withArguments("index-name",
-		file.getAbsolutePath());
-	verify(runtimeMock).exit(-3);
-    }
-
-    private void runMainWithDepentencies_withArguments(String... args) {
-	BucketFreezer bucketFreezer = BucketFreezer
-		.createWithDeafultSafeLocationAndHTTPClient();
-	bucketFreezer.httpClient = okReturningHttpClient;
-	BucketFreezer.runMainWithDepentencies(runtimeMock, bucketFreezer, args);
+	FileUtils.deleteQuietly(new File(getSafeLocation()));
     }
 
     public void createWithDeafultSafeLocationAndHTTPClient_initialize_nonNullValue() {
@@ -113,29 +39,22 @@ public class BucketFreezerTest {
 
     public void should_moveDirectoryToaSafeLocation_when_givenPath()
 	    throws IOException {
-	File safeLocationDirectory = null;
-	try {
-	    safeLocationDirectory = UtilsFile.createTempDirectory();
+	File safeLocationDirectory = UtilsFile.createTempDirectory();
 
-	    BucketFreezer bucketFreezer = new BucketFreezer(
-		    safeLocationDirectory.getAbsolutePath(),
-		    okReturningHttpClient, null);
-	    File dirToBeMoved = createTestDirectory();
+	BucketFreezer bucketFreezer = new BucketFreezer(
+		safeLocationDirectory.getAbsolutePath(), okReturningHttpClient,
+		null);
+	File dirToBeMoved = createTempDirectory();
 
-	    // Test
-	    int exitStatus = bucketFreezer.freezeBucket("index-name",
-		    dirToBeMoved.getAbsolutePath());
-	    assertEquals(0, exitStatus);
+	// Test
+	int exitStatus = bucketFreezer.freezeBucket("index-name",
+		dirToBeMoved.getAbsolutePath());
+	assertEquals(0, exitStatus);
 
-	    // Verify
-	    assertTrue(!dirToBeMoved.exists());
-	    assertTrue(safeLocationDirectory.exists());
-	    assertTrue(!isDirectoryEmpty(safeLocationDirectory));
-	} finally {
-	    if (safeLocationDirectory != null) {
-		FileUtils.deleteDirectory(safeLocationDirectory);
-	    }
-	}
+	// Verify
+	assertTrue(!dirToBeMoved.exists());
+	assertTrue(safeLocationDirectory.exists());
+	assertTrue(!isDirectoryEmpty(safeLocationDirectory));
     }
 
     public void freezeBucket_givenNonExistingSafeLocation_createSafeLocation()
@@ -145,7 +64,7 @@ public class BucketFreezerTest {
 	assertTrue(!nonExistingSafeLocation.exists());
 	BucketFreezer bucketFreezer = new BucketFreezer(notCreatedSafeLocation,
 		okReturningHttpClient, null);
-	File dirToBeMoved = createTestDirectory();
+	File dirToBeMoved = createTempDirectory();
 
 	// Test
 	bucketFreezer.freezeBucket("index", dirToBeMoved.getAbsolutePath());
@@ -157,25 +76,18 @@ public class BucketFreezerTest {
 
     public void freezeBucket_internalServerError_moveBucketToFailedBucketLocation()
 	    throws IOException {
-	File failedBucketLocation = null;
-	try {
-	    failedBucketLocation = UtilsFile.createTempDirectory();
-	    HttpClient failingHttpClient = UtilsMockito
-		    .createInternalServerErrorHttpClientMock();
-	    BucketFreezer bucketFreezer = new BucketFreezer(getSafeLocation(),
-		    failingHttpClient, failedBucketLocation.getAbsolutePath());
-	    File dirToBeMoved = createTestDirectory();
-	    assertFalse(!isDirectoryEmpty(failedBucketLocation));
+	File failedBucketLocation = UtilsFile.createTempDirectory();
+	HttpClient failingHttpClient = UtilsMockito
+		.createInternalServerErrorHttpClientMock();
+	BucketFreezer bucketFreezer = new BucketFreezer(getSafeLocation(),
+		failingHttpClient, failedBucketLocation.getAbsolutePath());
+	File dirToBeMoved = createTempDirectory();
+	assertFalse(!isDirectoryEmpty(failedBucketLocation));
 
-	    // Test
-	    bucketFreezer.freezeBucket("index", dirToBeMoved.getAbsolutePath());
+	// Test
+	bucketFreezer.freezeBucket("index", dirToBeMoved.getAbsolutePath());
 
-	    assertTrue(!isDirectoryEmpty(failedBucketLocation));
-	} finally {
-	    if (failedBucketLocation != null) {
-		FileUtils.deleteDirectory(failedBucketLocation);
-	    }
-	}
+	assertTrue(!isDirectoryEmpty(failedBucketLocation));
     }
 
     public void freezeBucket_internalServerError_createFailedBucketLocation()
@@ -190,15 +102,13 @@ public class BucketFreezerTest {
 	    assertTrue(!nonExistingLocation.exists());
 
 	    // Test
-	    bucketFreezer.freezeBucket("index", createTestDirectory()
+	    bucketFreezer.freezeBucket("index", createTempDirectory()
 		    .getAbsolutePath());
 
 	    assertTrue(nonExistingLocation.exists());
 
 	} finally {
-	    if (nonExistingLocation != null) {
-		FileUtils.deleteDirectory(nonExistingLocation);
-	    }
+	    FileUtils.deleteQuietly(nonExistingLocation);
 	}
     }
 
@@ -206,24 +116,7 @@ public class BucketFreezerTest {
      * This location is torn down by the AfterMethod annotation.
      */
     private String getSafeLocation() {
-	return System.getProperty("user.home") + "/" + getClass().getName();
-    }
-
-    /**
-     * This location is torn down by the AfterMethod annotation.
-     */
-    private File getTestDirectory() {
-	return new File(getClass().getSimpleName() + "-test-dir");
-    }
-
-    private File createTestDirectory() {
-	return createDirectory(getTestDirectory());
-    }
-
-    private File createDirectory(File dir) {
-	dir.mkdir();
-	assertTrue(dir.exists());
-	return dir;
+	return FileUtils.getUserDirectoryPath() + "/" + getClass().getName();
     }
 
 }
