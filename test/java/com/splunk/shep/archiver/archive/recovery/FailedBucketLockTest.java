8c48a235b2446316976730e86afa5abfe8379613
Petter Eriksson <periksson@splunk.com>
2012-03-07 23:27:48 -0800

    Created FailedBucketLock which is a subclass of SimpleFileLock.
    The FailedBucketLock is created without any arguments
    so that all instances of FailedBucketLock are
    accessing the same lock file.

---
+++ test/java/com/splunk/shep/archiver/archive/recovery/FailedBucketLockTest.java
@@ -0,0 +1,59 @@
+// Copyright (C) 2011 Splunk Inc.
+//
+// Splunk Inc. licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.splunk.shep.archiver.archive.recovery;
+
+import java.io.File;
+
+import org.apache.tools.ant.util.FileUtils;
+import org.testng.annotations.AfterMethod;
+
+import com.splunk.shep.testutil.UtilsFile;
+
+/**
+ * Fixture: new instance of FailedBucketsLock which is a subclass of
+ * SimpleFileLock.
+ */
+public class FailedBucketLockTest extends AbstractSimpleFileLockTest {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.splunk.shep.archiver.archive.recovery.AbstractSimpleFileLockTest#
+     * getSimpleFileLock()
+     */
+    @Override
+    protected SimpleFileLock getSimpleFileLock() {
+	return new FailedBucketLock();
+    }
+
+    @AfterMethod(groups = { "fast" })
+    public void removeFailedBucketLocksCreatedLockFile() {
+	File lockFile = FailedBucketLock.getLockFile();
+	FileUtils.delete(lockFile);
+	// In the creation of FailedBucketLock, it creates one parent to the
+	// lock file. Is this too much knowledge for this test?
+	File parentFile = lockFile.getParentFile();
+	if (UtilsFile.isDirectoryEmpty(parentFile)) {
+	    FileUtils.delete(parentFile);
+	} else {
+	    throw new RuntimeException("Something has changed! "
+		    + "This action is done because of the "
+		    + "assumption of that the lock file"
+		    + " creates one empty parent.");
+	}
+    }
+
+}
