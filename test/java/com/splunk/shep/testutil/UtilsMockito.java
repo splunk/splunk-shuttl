86006d53490aae6a315c64c351cbc93181fb4ede
Petter Eriksson <periksson@splunk.com>
2012-03-08 03:40:29 -0800

    UtilsMockito can create HttpClient with random HttpStatus return.
    This is useful when you don't care about the value
    of the status.

--- test/java/com/splunk/shep/testutil/UtilsMockito.java
+++ test/java/com/splunk/shep/testutil/UtilsMockito.java
@@ -3,6 +3,8 @@ package com.splunk.shep.testutil;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Random;
+
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
@@ -16,7 +18,7 @@ import org.apache.http.message.BasicHttpResponse;
 public class UtilsMockito {
 
     /**
-     * @return an HttpCLient that always returns HTTP OK coded responses when
+     * @return an HttpClient that always returns HTTP OK coded responses when
      *         the execute code is called.
      */
     public static HttpClient createAlwaysOKReturningHTTPClientMock() {
@@ -24,11 +26,38 @@ public class UtilsMockito {
     }
 
     /**
-     * @return an HttpCLient that always returns HTTP OK coded responses when
-     *         the execute code is called.
+     * @return an HttpClient that always returns HttpStatus Internal Error (500)
      */
     public static HttpClient createInternalServerErrorHttpClientMock() {
 	return createHttpClientMockReturningHttpStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * @return HttpClient that always random HttpStatus.
+     */
+    public static HttpClient createRandomHttpStatusHttpClient() {
+	HttpClient httpClient = mock(HttpClient.class);
+	StatusLine statusLine = mock(StatusLine.class);
+	try {
+	    when(httpClient.execute(any(HttpUriRequest.class))).thenReturn(
+		    new BasicHttpResponse(statusLine));
+	} catch (Exception e) {
+	    UtilsTestNG.failForException(
+		    "Could not assign return value for execute", e);
+	}
+	when(statusLine.getStatusCode()).thenReturn(getAnyHttpStatus());
+	return httpClient;
+    }
+
+    /**
+     * @return a fairly random HttpStatus
+     */
+    private static Integer getAnyHttpStatus() {
+	Integer[] someHttpStatuses = new Integer[] { HttpStatus.SC_ACCEPTED,
+		HttpStatus.SC_GATEWAY_TIMEOUT, HttpStatus.SC_OK,
+		HttpStatus.SC_INTERNAL_SERVER_ERROR };
+	Random rand = new Random();
+	return someHttpStatuses[rand.nextInt(someHttpStatuses.length)];
     }
 
     private static HttpClient createHttpClientMockReturningHttpStatus(
@@ -44,6 +73,6 @@ public class UtilsMockito {
 		    "Couldn't assign return value for execute", e);
 	}
 	return httpClient;
-
     }
+
 }
