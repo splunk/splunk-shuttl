3b1f4a0d1b1bf56bc6f2f6e080c13da5feb06a4a
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-03-21 01:16:58 -0700

    Removed standard error outputting.

--- src/java/com/splunk/shep/archiver/archive/ArchiveConfiguration.java
+++ src/java/com/splunk/shep/archiver/archive/ArchiveConfiguration.java
@@ -39,7 +39,6 @@ public class ArchiveConfiguration {
 	    sharedInstanceRef = new SoftReference<ArchiveConfiguration>(
 		    sharedInstance);
 	}
-	System.err.println(sharedInstanceRef);
 	return sharedInstance;
     }
 
