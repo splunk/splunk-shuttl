990a7635fc3d5c770b20db0a695162471164d109
André Eriksson <aeriksson@splunk.com>
2012-04-17 12:24:08 -0700

    fixed directory problem

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 rsync -vr ${DIR}/package/* ${DIR}/build-cache/splunk/etc/apps/shep/
-build-cache/splunk/bin/splunk restart -f 
+${DIR}/build-cache/splunk/bin/splunk restart -f 
