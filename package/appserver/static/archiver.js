37fcc5742664cb5b3b2dca791d8b252f04f156d2
Anton Jonsson <ajonsson@splunk.com>
2012-04-27 22:26:10 -0700

    Added date picker and form validation
    Also structured css style and overall functionality.

--- package/appserver/static/archiver.js
+++ package/appserver/static/archiver.js
@@ -31,6 +31,11 @@ function listBucketsGET() {
 }
 
 function listBucketsPOST() {
+
+  if (!$('#search-thaw-buckets-form').valid()) {
+    return;
+  }
+
   var formData = $('form').serializeArray();
   var data = formData;
   console.log(data);
@@ -45,13 +50,51 @@ function listBucketsPOST() {
   });
 }
 
+function getStaticContextURL() {
+  var url = Splunk.util.make_url('static/images/calendar_blue.png'); //initialize string
+  // var scripts = $('script').last().attr('src');
+  // url = scripts.slice(7, scripts.lastIndexOf("/"));
+  console.log(url);
+  return url;
+}
+
+// Splunk.util.getCurrentApp() only checks document.body not top.document
+function getAppName() {
+  return $(top.document.body).attr("s:app") || 'UNKNOWN_APP_REALLY';
+}
+
 $(document).ready(function() {
     bindHandler();
+
+    $('.datetime-input').datepicker({
+      dateFormat:"yy-mm-dd",
+      showOn: "button",
+      buttonImage: Splunk.util.make_url('static/app/', getAppName() ,'/images/calendar_blue.png'),
+      buttonImageOnly: true
+     });
+
+    $('#search-thaw-buckets-form').validate({
+      errorPlacement: function(error, element) {
+        error.insertAfter( element.next() );
+      }
+    });
+    jQuery.validator.addClassRules({
+      'datetime-input': {
+        required: true,
+        dateISO: true
+      }
+    });
+    $('.datetime-input').change( function() {
+      if ($('#search-thaw-buckets-form').valid()) {
+        enableSearchThawButton();
+      } else {
+        disableSearchThawButton();
+      }
+    });
 });
 
 function bindHandler() {
-    //$('.refresh').bind('click',function(event){alert('qwert!');});
-    $('#enable-button').bind('click', function() { toggleSearchThawButton(); });
+    //$('#enable-button').bind('click', function() { toggleSearchThawButton(); });
     $('#search-thaw-button').bind('click', function(event) { searchOrThawBuckets(event); });
 }
 
