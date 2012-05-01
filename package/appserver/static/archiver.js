e981e8630ad9e27cc48d1b708dc849dc14f335ad
Anton Jonsson <ajonsson@splunk.com>
2012-05-01 00:31:01 -0700

    Structural cleanup

--- package/appserver/static/archiver.js
+++ package/appserver/static/archiver.js
@@ -1,24 +1,3 @@
-function loadXMLDoc()
-{
-  var xmlhttp;
-  if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  }
-  else
-  {// code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange=function()
-  {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-      document.getElementById("bucket-table").innerHTML=xmlhttp.responseText;
-    }
-  }
-  xmlhttp.open("GET","/custom/shep/Archiving/list_buckets",true);
-  xmlhttp.send();
-}
 
 function listBucketsGET() {
   $.ajax({
