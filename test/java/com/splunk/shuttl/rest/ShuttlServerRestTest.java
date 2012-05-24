786e3bcd4cb068bf1d1bae88c0d2b7f1e1a5bdaf
Petter Eriksson <periksson@splunk.com>
2012-05-24 14:46:18 +0200

    Minimized the ivy dependencies. Close #20.

--- test/java/com/splunk/shuttl/rest/ShuttlServerRestTest.java
+++ test/java/com/splunk/shuttl/rest/ShuttlServerRestTest.java
@@ -16,67 +16,81 @@ package com.splunk.shuttl.rest;
 
 import static org.testng.Assert.*;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
-import javax.ws.rs.core.MediaType;
-
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
+import com.splunk.shuttl.ShuttlConstants;
 
+@Test(groups = { "end-to-end" })
 public class ShuttlServerRestTest {
-    // TODO: don't hardcode the default port/host
-    private static final String shuttlHost = "localhost";
-    private static final int shuttlMgmtPort = 9090;
-    private static String basePath = "/shuttl/rest/server";
-    private Client client;
-    
+
     @BeforeClass(groups = { "end-to-end" })
     public void setUpClient() {
-	System.out.println("*** Running ShuttlServerRestTest ***");
-	client = Client.create();
 	waitForServerToStart();
     }
 
     private void waitForServerToStart() {
 	try {
-	    Thread.sleep(5000);
+	    Thread.sleep(3000);
 	} catch (InterruptedException e) {
 	}
     }
 
-    @Test(groups = { "end-to-end" })
-    public void getDefaultHost() throws URISyntaxException {
-	URI defaultHostUri = new URI("http", null, shuttlHost, shuttlMgmtPort,
-		(basePath + "/defaulthost"), null, null);
+    @Parameters(value = { "shuttl.host", "shuttl.port" })
+    public void getDefaultHost(String shuttlHost, String shuttlPort)
+	    throws URISyntaxException, ClientProtocolException, IOException {
+	URI defaultHostUri = getUriForServerEndpoint(shuttlHost, shuttlPort,
+		ShuttlConstants.ENDPOINT_DEFAULT_HOST);
 
-	WebResource resource = client.resource(defaultHostUri);
-	ClientResponse response = resource.accept(MediaType.TEXT_PLAIN).get(
-		ClientResponse.class);
-	int status = response.getStatus();
-	String defaultHost = response.getEntity(String.class);
-	assertEquals(status, 200, "Unexpected status. response=" + response);
-	assertEquals(defaultHost, "localhost",
-		"Unexpected defaultHost. response="
-		+ response);
+	HttpResponse response = getResponseFromShuttlServer(defaultHostUri);
+	List<String> lines = getLinesFromResponse(response);
+	assertEquals(1, lines.size());
+	assertEquals("localhost", lines.get(0));
     }
 
-    @Test(groups = { "end-to-end" })
-    public void getDefaultPort() throws URISyntaxException {
-	URI defaultPortUri = new URI("http", null, shuttlHost, shuttlMgmtPort,
-		(basePath + "/defaultport"), null, null);
-
-	WebResource resource = client.resource(defaultPortUri);
-	ClientResponse response = resource.accept(MediaType.TEXT_PLAIN).get(
-		ClientResponse.class);
-	int status = response.getStatus();
-	int defaultPort = Integer.parseInt(response.getEntity(String.class));
-	assertEquals(status, 200, "Unexpected status. response=" + response);
-	assertEquals(defaultPort, 9000, "Unexpected defaultPort, response="
-		+ response);
+    private URI getUriForServerEndpoint(String shuttlHost, String shuttlPort,
+	    String defaultPortEndpoint) throws URISyntaxException {
+	String serverEndpoints = "/" + ShuttlConstants.ENDPOINT_CONTEXT
+		+ ShuttlConstants.ENDPOINT_SERVER;
+	return new URI("http", null, shuttlHost, Integer.parseInt(shuttlPort),
+		serverEndpoints + defaultPortEndpoint, null, null);
     }
+
+    private HttpResponse getResponseFromShuttlServer(URI requestUri)
+	    throws IOException, ClientProtocolException {
+	HttpGet httpGet = new HttpGet(requestUri);
+	HttpResponse response = new DefaultHttpClient().execute(httpGet);
+	int status = response.getStatusLine().getStatusCode();
+	assertEquals(200, status, "Unexpected status. response=" + response);
+	return response;
+    }
+
+    private List<String> getLinesFromResponse(HttpResponse response)
+	    throws IOException {
+	return IOUtils.readLines(response.getEntity().getContent());
+    }
+
+    @Parameters(value = { "shuttl.host", "shuttl.port" })
+    public void getDefaultPort(String shuttlHost, String shuttlPort)
+	    throws URISyntaxException, IOException {
+	URI defaultPortUri = getUriForServerEndpoint(shuttlHost, shuttlPort,
+		ShuttlConstants.ENDPOINT_DEFAULT_PORT);
+
+	HttpResponse response = getResponseFromShuttlServer(defaultPortUri);
+	List<String> lines = getLinesFromResponse(response);
+	assertEquals(1, lines.size());
+	assertEquals(9000, Integer.parseInt(lines.get(0)));
+    }
+
 }
