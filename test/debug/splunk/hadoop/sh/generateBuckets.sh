0c9fef7541a608cbf03c31bcf3a03cb7663ed14b
Anton Jonsson <anton.t.jonsson@gmail.com>
2012-08-22 02:26:27 -0700

    Made generateBuckets script move generic.

--- test/debug/splunk/hadoop/sh/generateBuckets.sh
+++ test/debug/splunk/hadoop/sh/generateBuckets.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
+argsLength=$#
+if [[ $argsLength < 3 ]]; then
+    echo "Not enought arguments!"
+    exit 1
+fi
+
 splunkPath=$1
 filePath=$2
 index=$3
+if [[ $argsLength == 4 ]]; then
+    login=$4
+else
+    login="admin:changeme"
+fi
 loremIpsum=$filePath/loremIpsum.txt
+
 
 echo "\$0 = $0"
 echo "\$splunkPath = $splunkPath"
 echo "\$filePath = $filePath"
 echo "\$loremIpsum = $loremIpsum"
 echo "\$index = $index"
-echo ""
+echo "\$login = $login"
+echo "----------------------------"
 
 ret_code=0
 if [[ ! -f $loremIpsum ]]; then
@@ -28,7 +41,7 @@ fi
 
 i=0
 while [ $i -lt 20 -a $ret_code -eq 0 ]; do 
-	$splunkPath/bin/splunk add oneshot $loremIpsum -index $index || ret_code=1
+	$splunkPath/bin/splunk add oneshot $loremIpsum -index $index -auth $login || ret_code=1
 	let i++
 done
 
