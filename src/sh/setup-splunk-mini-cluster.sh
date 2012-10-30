f6b773988dd0b1e44c8fd013410059ae1e2f26db
Petter Eriksson <periksson@splunk.com>
2012-10-30 12:47:34 -0700

    #Integrates splunk cluster setup .sh with new cluster ant targets. #95

--- src/sh/setup-splunk-mini-cluster.sh
+++ src/sh/setup-splunk-mini-cluster.sh
@@ -28,4 +28,28 @@ setup-splunk-instance $MASTER_NAME $MASTER_PORT
 setup-splunk-instance $SLAVE1_NAME $SLAVE1_PORT
 setup-splunk-instance $SLAVE2_NAME $SLAVE2_PORT
 
+make-cluster-master() {
+  server_name=$1
+  mgmt_port=$2
+  call_ant_with_string_args "\
+      -Dsplunk.mgmtport=$mgmt_port \
+      -Dsplunk.server.name=$server_name \
+      splunk-make-cluster-master-with-replication-2"
+}
+
+make-cluster-slave() {
+  server_name=$1
+  mgmt_port=$2
+  replication_port=$3
+  call_ant_with_string_args "\
+      -Dsplunk.mgmtport=$mgmt_port \
+      -Dsplunk.server.name=$server_name \
+      -Dcluster.replication.port=$replication_port \
+      -Dcluster.master.uri=https://localhost:$MASTER_PORT \
+      splunk-make-slave"
+}
+
+make-cluster-master $MASTER_NAME $MASTER_PORT
+make-cluster-slave $SLAVE1_NAME $SLAVE1_PORT 8214
+make-cluster-slave $SLAVE2_NAME $SLAVE2_PORT 8215
 
