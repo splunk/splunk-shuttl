cca118564ea1b763ab49d0eec8475191bbb77c5a
Petter Eriksson <periksson@splunk.com>
2012-10-31 13:34:44 -0700

    Fixes grep error, when Latest bundle is  instead of a hash. #95

--- src/sh/wait-for-new-cluster-bundle.sh
+++ src/sh/wait-for-new-cluster-bundle.sh
@@ -9,8 +9,12 @@ number_latest=0
 
 while [ $number_latest != 4 ]; do
   cluster_status="`$splunk show cluster-bundle-status`"
-  latest_bundle=`echo "$cluster_status" | grep "Latest" | head -n 1 | awk '{print $3}'`
-  number_latest=`echo "$cluster_status" | grep $latest_bundle | wc -l`
-  sleep 1
+  latest_bundle=`echo "$cluster_status" |
+    grep "Latest" |
+    awk '{print $3}' |
+    sed '/^$/d' |
+    head -n 1`
+  number_latest=`echo "$cluster_status" | grep $latest_bundle 2> /dev/null | wc -l`
+  sleep 2
 done
 
