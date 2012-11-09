492f128108285607fa1ff9412abc474251ed380d
Petter Eriksson <periksson@splunk.com>
2012-11-08 18:32:22 -0800

    Custom REST endpoint prints the configured shuttl server port. #79

--- package/bin/shuttl_rest_handler.py
+++ package/bin/shuttl_rest_handler.py
@@ -1,15 +1,20 @@
 import splunk
 import os
+from xml.dom import minidom
 
 BIN_DIR = os.path.abspath(os.path.dirname(__file__))
 APP_DIR = os.path.abspath(os.path.join(BIN_DIR, '..'))
+CONF_DIR = os.path.abspath(os.path.join(APP_DIR, 'conf'))
+SHUTTL_CONF = os.path.abspath(os.path.join(CONF_DIR, 'server.xml'))
 
 class RendersPort(splunk.rest.BaseRestHandler):
 
     def handle_GET(self):
         response = self.response
-        request = self.request
-        form = request['query']
-        #self.settings = settings = splunk.clilib.cli_common.getConfStanza('pdf_server', 'settings')
-        response.write('hello interwebz')
+        response.write('Shuttl Port: ' + self._getShuttlPort())
+
+    def _getShuttlPort(self):
+        xmldoc = minidom.parse(SHUTTL_CONF)
+        httpPortNode = xmldoc.getElementsByTagName('httpPort')[0]
+        return httpPortNode.childNodes[0].nodeValue
 
