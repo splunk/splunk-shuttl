0fabb7eded966b908b18cb3e0cf3c0b2c8ea6873
Petter Eriksson <periksson@splunk.com>
2012-11-24 02:21:19 -0800

    copyBucket.sh takes the right parameters, bucket_path and cold_path. #80

--- package/bin/copyBucket.sh
+++ package/bin/copyBucket.sh
@@ -3,9 +3,10 @@
 set -e
 set -u
 
-index="$1"
+cd $SPLUNK_HOME/etc/apps/shuttl/bin
+
 bucket="$2"
 cold_path_destination="$3"
 
 source java_executable.env
-exec $JAVA -cp ./*:../lib/* com.splunk.shuttl.archiver.copy.CopiesBuckets "$index" "$bucket" "$cold_path_destination"
+exec $JAVA -cp ./*:../lib/* com.splunk.shuttl.archiver.copy.CopiesBuckets "$bucket" "$cold_path_destination"
