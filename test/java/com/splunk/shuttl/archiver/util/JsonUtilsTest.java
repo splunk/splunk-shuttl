baeaff9fea62faf6964f783770e4b85471c69047
Petter Eriksson <periksson@splunk.com>
2013-01-10 15:47:27 +0100

    Can merge JSON objects by key. #38

---
+++ test/java/com/splunk/shuttl/archiver/util/JsonUtilsTest.java
@@ -0,0 +1,80 @@
+// Copyright (C) 2011 Splunk Inc.
+//
+// Splunk Inc. licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.splunk.shuttl.archiver.util;
+
+import static java.util.Arrays.*;
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
+
+@Test(groups = { "fast-unit" })
+public class JsonUtilsTest {
+
+	public void merge_jsonWithSameKeyDifferentValues_jsonArrayWithBothValues()
+			throws JSONException {
+		JSONObject o1 = json("{ \"k\" : 1 }");
+		JSONObject o2 = json("{ \"k\" : 2 }");
+		JSONObject merge = JsonUtils.mergeKey(asList(o1, o2), "k");
+		assertEquals(merge.get("k").toString(), "[1,2]");
+		assertEquals("{\"k\":[1,2]}", merge.toString());
+	}
+
+	public void merge_identicalJsons_addsBothValues() throws JSONException {
+		JSONObject o1 = json("{ \"k\" : 1 }");
+		JSONObject o2 = json("{ \"k\" : 1 }");
+		JSONObject merge = JsonUtils.mergeKey(asList(o1, o2), "k");
+		assertEquals(merge.get("k").toString(), "[1,1]");
+	}
+
+	public void merge_jsonWithDifferentKeys_otherKeyDoesNotExist()
+			throws JSONException {
+		JSONObject o1 = json("{ \"k\" : 1 }");
+		JSONObject o2 = json("{ \"k\" : 1, \"j\" : 2 }");
+		JSONObject merge = JsonUtils.mergeKey(asList(o1, o2), "k");
+		assertEquals(merge.get("k").toString(), "[1,1]");
+		try {
+			merge.get("j");
+			fail();
+		} catch (JSONException e) {
+		}
+	}
+
+	public void merge_jsonWithArrays_addsAllValuesToTheSameArray()
+			throws JSONException {
+		JSONObject o1 = json("{ \"k\" : [1,1] }");
+		JSONObject o2 = json("{ \"k\" : [2,2] }");
+		JSONObject o3 = json("{ \"k\" : 3 }");
+		JSONObject merge = JsonUtils.mergeKey(asList(o1, o2, o3), "k");
+		assertEquals(merge.get("k").toString(), "[1,1,2,2,3]");
+	}
+
+	public void merge_oneEmptyJson_equalsNonEmptyOne() throws JSONException {
+		JSONObject o1 = json("{ \"k\" : 1 }");
+		JSONObject o2 = json("{ }");
+		JSONObject merge = JsonUtils.mergeKey(asList(o1, o2), "k");
+		assertEquals(merge.get("k").toString(), "1");
+	}
+
+	private JSONObject json(String s) {
+		try {
+			return new JSONObject(s);
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
