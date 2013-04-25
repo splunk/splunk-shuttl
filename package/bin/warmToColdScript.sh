fc22a804cb08c64c8ca7d3c769bc2293ce1aa517
Petter Eriksson <periksson@splunk.com>
2013-04-25 13:06:25 -0700

    warmToColdScript.sh is not blocking until the operation is complete
    Searches and other activities are paused while
    the warmToColdScript is running, so we don't
    block.

--- package/bin/warmToColdScript.sh
+++ package/bin/warmToColdScript.sh
@@ -38,4 +38,4 @@ cold_path_destination="$2"
 mv $bucket $cold_path_destination
 
 source java_executable.env
-exec $JAVA -cp ./*:../lib/* com.splunk.shuttl.archiver.copy.ColdCopyEntryPoint "$cold_path_destination"
+$JAVA -cp ./*:../lib/* com.splunk.shuttl.archiver.copy.ColdCopyEntryPoint "$cold_path_destination" &
