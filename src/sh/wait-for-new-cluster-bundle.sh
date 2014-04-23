dbdfc7b64f119a59dd459a8f7f133f154476d34d
Petter Eriksson <periksson@splunk.com>
2014-04-23 16:16:40 -0700

    Updated waiting for new cluster bundle script to match Splunk 6.x

--- src/sh/wait-for-new-cluster-bundle.sh
+++ src/sh/wait-for-new-cluster-bundle.sh
@@ -7,13 +7,12 @@ SPLUNK_HOME="$1"
 splunk="$SPLUNK_HOME"/bin/splunk
 number_latest=0
 
-while [ $number_latest != 4 ]; do
+while [ $number_latest != 6 ]; do
   cluster_status="`$splunk show cluster-bundle-status`"
   latest_bundle=`echo "$cluster_status" |
-    grep "Latest" |
-    awk '{print $3}' |
-    sed '/^$/d' |
-    head -n 1`
+    head -n 4 | 
+    tail -n 1 |
+    cut -d = -f 2`
   number_latest=`echo "$cluster_status" | grep $latest_bundle 2> /dev/null | wc -l`
   sleep 2
 done
